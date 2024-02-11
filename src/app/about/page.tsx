a3b4fa6bda0fd13fd503af1b317967880434db0b
DeveloperMajd <developermajd@gmail.com>
2024-02-11 17:42:33 +0100

    create textmedia component

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,12 +1,15 @@
 import { Navbar } from "../components/Navbar";
 import { Footer } from "../components/Footer";
 import { TextMediaBanner } from "../components/TextMediaBanner";
+import { TextMedia } from "../components/TextMedia";
 
 const about = () => {
   return (
     <div>
       <Navbar />
       <TextMediaBanner />
+      <TextMedia data="img-left" />
+      <TextMedia data="img-right" />
       <Footer />
     </div>
   );
